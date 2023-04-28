15cce1b20133d78c91177c166dfe634a87d2a6f1
alexowoade <alexowoade@gmail.com>
2023-04-28 01:48:30 +0000

    alex

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -4,7 +4,7 @@ from typing import Sequence, Any, Union
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
-    """Returns the first element of a sequence, if it exists. Otherwise, returns None."""
+    """Augment the following code with the correct duck-typed annotations:"""
     if lst:
         return lst[0]
     else:
