74d2a52cbbb44310443bc81c41d053569c7eadd6
alexowoade <alexowoade@gmail.com>
2023-04-28 02:15:44 +0000

    python

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python3
-'''Type annotated function to add two floats'''
+'''Basic annotations - add'''
 
 
 def add(a: float, b: float) -> float:
-    """
-    Adds two floats.
-
-    Args:
-    a (float): A float variable.
-    b (float): A float variable.
-
-    Returns:
-    float: The sum of a and b.
+    """adds two numbers using type annotation
+    Keyword arguments:
+    a -- float variable
+    b -- float variable
+    Return: sum of a and b as float
     """
     return a + b
 
