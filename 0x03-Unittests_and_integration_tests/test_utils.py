b90e9d5c06fba9f02a86833fa4b493288d341324
alexowoade <alexowoade@gmail.com>
2023-05-22 22:47:23 +0000

    alex

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python3
 """
-<<<<<<< HEAD
 This module contains unit tests for the access_nested_map function in utils.py.
-=======
-Unittests for utils.access_nested_map function.
->>>>>>> 6b06815783610bf670cda836ff9154801a4208df
 """
 
 import unittest
@@ -13,31 +9,19 @@ from utils import access_nested_map
 
 class TestAccessNestedMap(unittest.TestCase):
     """
-<<<<<<< HEAD
     TestAccessNestedMap class contains unit tests for access_nested_map function.
-=======
-    Test case for access_nested_map function.
->>>>>>> 6b06815783610bf670cda836ff9154801a4208df
     """
 
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
-<<<<<<< HEAD
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(self, nested_map, path, expected_result):
-=======
-        ({"a": {"b": 2}}, ("a", "b"), 2)
-    ])
-    def test_access_nested_map(self, nested_map: Dict[str, Any], path: Tuple[str, ...],
-                               expected_result: Any) -> None:
->>>>>>> 6b06815783610bf670cda836ff9154801a4208df
         """
         Test access_nested_map function with different inputs.
 
         Args:
-<<<<<<< HEAD
             nested_map (dict): The nested map.
             path (tuple): The path to access the value in the nested map.
             expected_result: The expected result after accessing the nested map.
@@ -47,20 +31,3 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         self.assertEqual(access_nested_map(nested_map, path), expected_result)
 
-=======
-            nested_map: The nested map to access.
-            path: The path to the desired value within the map.
-            expected_result: The expected result of accessing the nested map.
-
-        Returns:
-            None.
-
-        Raises:
-            AssertionError: If the actual result doesn't match the expected result.
-        """
-        self.assertEqual(access_nested_map(nested_map, path), expected_result)
-
-if __name__ == '__main__':
-
-
->>>>>>> 6b06815783610bf670cda836ff9154801a4208df
